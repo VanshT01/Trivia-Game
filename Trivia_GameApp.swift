8694612e8ac2e1af46d6621dc72b9610cd28a332
VanshT01 <69682187+VanshT01@users.noreply.github.com>
2024-03-11 20:54:38 -0400

    Create Trivia_GameApp.swift

---
+++ Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Vansh Talreja on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
